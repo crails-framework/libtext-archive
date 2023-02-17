0db5b7017a1156e1630da472eece77cad39ea92b
plaristote <michael@unetresgrossebite.com>
2023-02-17 12:18:50 +0100

    adds const qualifier on serialize method

--- libtext-archive/archivable.hpp
+++ libtext-archive/archivable.hpp
@@ -7,12 +7,13 @@ class IArchive;
 struct Archivable
 {
   virtual void serialize(IArchive&) = 0;
-  virtual void serialize(OArchive&) = 0;
+  virtual void serialize(OArchive&) const = 0;
 };
 
 # define define_serializer(body) \
-  template<typename ARCHIVE> \
-  void serialize(ARCHIVE& archive) \
-  { body }
+  void serialize(IArchive& archive) override { archive & body; } \
+  void serialize(OArchive& archive) const override { archive & body; }
+
+# define ARCHIVE_SERIALIZER(body) define_serializer(body)
 
 #endif
